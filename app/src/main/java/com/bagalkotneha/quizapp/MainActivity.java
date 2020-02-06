a3064a9c524dc78019e442248c63b3853165490b
Bagalkot <1707612@fcps.edu>
2020-02-06 10:16:32 -0500

    Initial commit

---
+++ app/src/main/java/com/bagalkotneha/quizapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.bagalkotneha.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button submitButton;
+    EditText responseText;
+    TextView displayText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        submitButton = findViewById(R.id.clickButton);
+        responseText = findViewById(R.id.responseEditText);
+        displayText = findViewById(R.id.textBox);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello mom!");
+                Log.i("firstButton", "Hi mom!");
+                Log.i("testButton","Hi dad!");
+                Log.i("testButton", responseText.getText().toString());
+                displayText.setText(displayText.getText().toString()+" "+responseText.getText().toString());
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    if (responseText.getText().toString() == "TJ") {
+                        displayText.setText("TJ Rocks!");
+                    }
+                }
+            }
+        });
+    }
+}
